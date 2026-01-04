9e9a70128cf06022cc2fc562277bb9eb98c13fe3
Couleur <201435454@qq.com>
2026-01-04 16:10:36 +0800

    chore: 更新 .gitignore 文件以排除 .waylog，更新 AppFooter 组件以添加备案信息链接

--- src/components/layout/AppFooter.vue
+++ src/components/layout/AppFooter.vue
@@ -2,6 +2,15 @@
   <footer class="mt-8 border-t border-gray-200/60 dark:border-gray-800/60">
     <div class="container mx-auto px-6 py-6 text-center text-xs text-gray-500 dark:text-gray-400">
       <span>© {{ currentYear }} Couleur X. All rights reserved.</span>
+      <span class="mx-2">|</span>
+      <a 
+        href="https://beian.miit.gov.cn/" 
+        target="_blank" 
+        rel="noopener noreferrer"
+        class="hover:text-gray-700 dark:hover:text-gray-200 transition-colors"
+      >
+        粤ICP备2026000260号
+      </a>
     </div>
   </footer>
   
